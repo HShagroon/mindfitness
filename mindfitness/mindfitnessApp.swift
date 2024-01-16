4d4906e90902cb08b42ec2f3560c968af611b74e
HShagroon <haneenshagroon@gmail.com>
2024-01-16 09:38:35 +0300

    Initial Commit

---
+++ mindfitness/mindfitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  mindfitnessApp.swift
+//  mindfitness
+//
+//  Created by Haneen Rida Shagroon on 04/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct mindfitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
